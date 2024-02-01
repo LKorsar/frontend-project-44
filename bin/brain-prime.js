1b8db80836cb000a7657c8cc3b753382fec63d33
Мария Акимкина <wapovalova@gmail.com>
2024-02-01 14:51:20 +0300

    add bin/brain-prime.js, update Makefile and package.json

---
+++ bin/brain-prime.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/* eslint-disable import/extensions */
+import primeGame from '../src/games/prime.js';
+/* eslint-enable import/extensions */
+
+/* eslint-disable no-console */
+console.log('brain-prime');
+/* eslint-enable no-console */
+
+primeGame();
