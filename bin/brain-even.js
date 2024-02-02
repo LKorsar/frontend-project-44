1d1dcece34e57075e856dc601e13f3a2324fc3ad
Мария Акимкина <wapovalova@gmail.com>
2024-02-02 17:04:54 +0300

    update bin/brain-even.js, src/games/even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable import/extensions */
 import evenGame from '../src/games/even.js';
-/* eslint-enable import/extensions */
-
-/* eslint-disable no-console */
-console.log('brain-even');
-/* eslint-enable no-console */
 
 evenGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,19 @@
-/* eslint-disable import/extensions */
 import commonGameLogic from '../index.js';
 import getRandomInt from '../randomInt.js';
-/* eslint-enable import/extensions */
 
 const evenGame = () => {
+  console.log('brain-even');
   const evenGameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
   const evenGameAlgorithm = () => {
     const number = getRandomInt(1, 1000);
     const gameQuestion = number;
     let correctAnswer;
-    /* eslint-disable no-unused-expressions */
-    number % 2 === 0 ? correctAnswer = 'yes' : correctAnswer = 'no';
-    /* eslint-enable no-unused-expressions */
-
+    if (number % 2 === 0) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
     return [gameQuestion, correctAnswer];
   };
   commonGameLogic(evenGameRules, evenGameAlgorithm);
