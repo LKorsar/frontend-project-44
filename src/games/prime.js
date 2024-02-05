b03431fcd665548c5532b52cfc2a5c421dc6a8f9
Мария Акимкина <wapovalova@gmail.com>
2024-02-05 16:28:43 +0300

    update bin/brain-prime.js and src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,8 +1,7 @@
-import commonGameLogic from '../index.js';
+import getCommonGameLogic from '../index.js';
 import getRandomInt from '../randomInt.js';
 
-const primeGame = () => {
-  console.log('brain-prime');
+const getPrimeGame = () => {
   const primeGameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const primeGameAlgorithm = () => {
@@ -25,7 +24,7 @@ const primeGame = () => {
     }
     return [gameQuestion, correctAnswer];
   };
-  commonGameLogic(primeGameRules, primeGameAlgorithm);
+  getCommonGameLogic(primeGameRules, primeGameAlgorithm);
 };
 
-export default primeGame;
+export default getPrimeGame;
